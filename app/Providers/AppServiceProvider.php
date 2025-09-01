e1782e4a67ff76e1100f6ed60edbffab5eb25f55
Rafael Miano <142014760+rafael-miano@users.noreply.github.com>
2025-09-01 21:36:55 +0800

    added register and logoutresponse

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Filament\Support\Facades\FilamentView;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
-use Filament\Http\Responses\Auth\Contracts\LoginResponse as LoginResponseContract;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,11 +14,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+
+        $this->app->singleton(
+            \Filament\Http\Responses\Auth\Contracts\LogoutResponse::class,
+            \App\Http\Responses\LogoutResponse::class
+        );
+
+        $this->app->singleton(
+            \Filament\Http\Responses\Auth\Contracts\LoginResponse::class,
+            \App\Http\Responses\LoginResponse::class
+        );
+
+        $this->app->singleton(
+            \Filament\Http\Responses\Auth\Contracts\RegistrationResponse::class,
+            \App\Http\Responses\RegisterResponse::class
+        );
+
+
         //e inject ang compiled javascript kung magrun sa terminal ug npm run dev or build to the admin panel
         parent::register();
         FilamentView::registerRenderHook('panels::body.end', fn(): string => Blade::render("@vite('resources/js/app.js')"));
 
-        $this->app->bind(LoginResponseContract::class, \App\Http\Responses\LoginResponse::class);
+
     }
 
     /**
